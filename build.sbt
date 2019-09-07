1bbbd91572fb9bc0f544bc4a27948e045cd89457
palbook27 <palbook27@naver.com>
2019-09-07 13:20:54 +0900

    init set

--- build.sbt
+++
@@ -1,5 +0,0 @@
-name := "scalaTemp"
-
-version := "0.1"
-
-scalaVersion := "2.13.0"
--- src/main/scala/MainScala.scala
+++
@@ -1,7 +0,0 @@
-object MainScala {
-
-  def main(args: Array[String]): Unit = {
-
-    println("Hello world")
-  }
-}
